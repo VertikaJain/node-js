d4e3f6760129fbc130e5098eec64d33120f72bf0
Vertika Jain <vertikajain96@gmail.com>
2020-10-25 17:14:00 +0530

    JS Prototype Property #Day85

--- constructorFunction.js
+++ constructorFunction.js
@@ -3,11 +3,23 @@ function User(email, name) {
     this.email = email
     this.name = name
     this.online = false
-    this.login = function () {
-        console.log(this.email, "has logged in");
-    }
 }
+
+// prototype -> points to the __proto__ property
+User.prototype.login = function () {
+    this.online = true
+    console.log(this.email, "has logged in");
+}
+User.prototype.logout = function () {
+    this.online = false
+    console.log(this.email, "has logged out");
+}
+
 let user1 = new User("abc@gmail.com", "abc")
 let user2 = new User("xyz@gmail.com", "xyz")
 console.log(user1);
 user2.login()
+console.log(user2);
+user2.logout()
+console.log(user2);
+// You cannot manipulate prototype property of user1 or user2 object. It can be only used for the Constructor fn. User
