3bcb147202f631309a47fa0da63431f3108fc76d
Vertika Jain <vertikajain96@gmail.com>
2020-10-25 18:07:49 +0530

    JS Prototype Inheritance #Day85

--- constructorFunction.js
+++ constructorFunction.js
@@ -23,3 +23,23 @@ console.log(user2);
 user2.logout()
 console.log(user2);
 // You cannot manipulate prototype property of user1 or user2 object. It can be only used for the Constructor fn. User
+
+// Prototype Inheritance
+function Admin(...args) { //Rest parameter
+    User.apply(this, args) // The apply() method calls a fn with a given this value and arguments provided as an array
+    this.role = "super admin" // additional properties
+}
+
+Admin.prototype = Object.create(User.prototype) // Creating obj of specified prototype-> Also implements prototype chaining
+
+Admin.prototype.deleteUser = function (user) { // additional property of admin
+    Users = Users.filter(u => u.email != user.email)
+}
+
+let admin = new Admin("admin@gmail.com", "admin")
+console.log(admin);
+admin.login()
+let Users = [user1, user2, admin]
+console.log("Before deleting user: ", Users);
+admin.deleteUser(user2)
+console.log("After deleting user: ", Users);
